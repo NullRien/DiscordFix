5037ddd50210665f5319e09f118fa6f4938d5344
NullRien <chrismissick@outlook.com>
2022-10-26 20:00:50 -0400

    remove local appdata discord because i forgert

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from os import path, remove, getenv
 from sys import stdout, exit
 from time import sleep
+import os
+import sys
 
 discord_path = path.join(getenv('APPDATA'), 'discord')
+localappdata = path.join(getenv('LOCALAPPDATA'), 'discord')
 
 if path.exists(discord_path):
     remove(discord_path)
+    remove(localappdata)
     print('Discord folder removed')
     for char in "You are free. Now go outside.": 
         print(char, end='') 
@@ -15,4 +19,3 @@ if path.exists(discord_path):
 if not path.exists(discord_path):
     print('Discord not found')
     exit(1)
-
